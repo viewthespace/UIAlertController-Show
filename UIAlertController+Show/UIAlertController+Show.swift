7f594f3ec4747a4adcc644b9ac0011570552bf6d
Erik Ackermann <erik@gethightower.com>
2016-01-20 11:05:52 -0500

    Implement first version of show extension.

--- UIAlertController+Show/UIAlertController+Show.swift
+++ UIAlertController+Show/UIAlertController+Show.swift
@@ -7,7 +7,33 @@
 //
 
 import Foundation
+import ObjectiveC
 
-extension UIAlertController {
+private var alertWindowAssociationKey: UInt8 = 0
 
+public extension UIAlertController {
+
+    var alertWindow: UIWindow? {
+        get {
+            return objc_getAssociatedObject(self, &alertWindowAssociationKey) as? UIWindow
+        }
+        set(newValue) {
+            objc_setAssociatedObject(self, &alertWindowAssociationKey, newValue, objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+        }
+    }
+
+    public func show(animated: Bool = true) {
+        self.alertWindow = UIWindow(frame: UIScreen.mainScreen().bounds)
+        self.alertWindow?.rootViewController = UIViewController()
+        self.alertWindow?.windowLevel = UIWindowLevelAlert + 1
+        self.alertWindow?.makeKeyAndVisible()
+        self.alertWindow?.rootViewController?.presentViewController(self, animated: animated, completion: nil)
+    }
+
+    override public func viewDidDisappear(animated: Bool) {
+        super.viewDidDisappear(animated)
+
+        self.alertWindow?.hidden = true
+        self.alertWindow = nil
+    }
 }
